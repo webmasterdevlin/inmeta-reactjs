2b1ca362ba8a16b8fc758af1fad993f4eec49400
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-11-23 15:18:26 +0100

    created useeffect inside memberlist

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function MemberList() {
+
+    const [members, setMembers] = useState([]);
+    useEffect(() => {
+        loadMembers();
+    }, []);
+
+    const loadMembers = async () => {
+        const response = await axios.get("http://localhost:5000/members");
+        console.log(response.data);
+    }
+
     return (<h1>MemberList Works!</h1>)
 }
