3ce5fa0c8e83cda8713f05539b0155e9058c07ff
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-11-24 10:04:51 +0100

    added POC post request

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 export default function MemberList() {
     const [members, setMembers] = useState([]);
+    const [member, setMember] = useState({});
+
     useEffect(() => {
         loadMembers();
     }, []);
@@ -21,16 +24,33 @@ export default function MemberList() {
         setMembers(newMembers);
 
         try {
-            await axios.delete(`http://localhost:5000/members/${id}xx44`);
+            await axios.delete(`http://localhost:5000/members/${id}3247328`);
         } catch (err) {
             alert(err.message);
             setMembers(previous);
         }
     }
 
+    const handleAddMember = () => {
+        console.log("MEMBER: ", member)
+    }
+
+    const handleNameOnChange = ({ currentTarget }) => {
+        console.log(currentTarget.value)
+        setMember({ ...member, name: currentTarget.value })
+    }
+
+    const handleAgeOnChange = ({ currentTarget }) => {
+        console.log(currentTarget.value);
+        setMember({ ...member, age: currentTarget.value })
+    }
+
     return (<div style={{ width: '75vw', margin: "0 auto", padding: '2rem' }}>
         <h2>React Demo</h2>
         <h3>
+            <TextField onChange={handleNameOnChange} margin="dense" variant="outlined" multiline rowsMax="4" />
+            <TextField onChange={handleAgeOnChange} margin="dense" variant="outlined" multiline rowsMax="4" />
+            <Button variant="contained" color="primary" onClick={handleAddMember} >Add Member</Button>
             <ul style={{ listStyle: 'none' }}>
                 {members.map(m =>
                     <Box key={m.id} boxShadow={3}>
