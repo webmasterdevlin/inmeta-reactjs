103c0cdcc9d7d4d3d93e845c788a3268f3dba292
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-11-24 15:11:40 +0100

    can navigate

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
-export default function Profile() {
-    return (<h1>Profile Works!</h1>)
+const useStyles = makeStyles({
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
+export default function Profile(params) {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+
+    const [member, setMember] = useState({})
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        console.log('PROPS: ', params)
+        getMemberById();
+    }, [])
+
+    const getMemberById = async () => {
+        setLoading(true)
+        const { data } = await axios.get(`http://localhost:5000/members/${params.id}`)
+        setMember(data);
+        setLoading(false)
+    }
+
+    return (
+        <Card className={classes.card}>
+            {loading
+                ? <h1>Loading...</h1>
+                : <div>
+                    <CardContent>
+                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                            Word of the Day
+        </Typography>
+                        <Typography variant="h5" component="h2">
+                            {member.name}
+                        </Typography>
+                        <Typography className={classes.pos} color="textSecondary">
+                            adjective
+        </Typography>
+                        <Typography variant="body2" component="p">
+                            well meaning and kindly.
+          <br />
+                            {'"a benevolent smile"'}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </div>
+            }
+        </Card>
+    );
 }
